3d0e6ce8f650df3dd50ca0daea9e05ec84df513a
debashish2549 <debashish.deep74@qmail.cuny.edu>
2016-06-12 14:28:47 -0400

    first commit reading the file

---
+++ main.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+#include<fstream>
+using namespace std;
+
+ 
+
+int main(int argc, const char * argv[]) {
+    ifstream infile;
+    infile.open ("/Users/deep/desktop/input.txt");
+    int x;
+    while (infile >> x) {
+        cout << x << endl;
+    }
+    if (infile.is_open())    cout << "Hello, World!\n";
+    infile.close();
+    return 0;
+}
