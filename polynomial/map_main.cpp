a09949fdc3e13a80f84a61ab1eb03dd5508f4e68
Debashish Saha <ashish.saha01@gmail.com>
2016-06-28 00:00:55 -0400

    Update map_main.cpp
    finished with input output file

--- polynomial/map_main.cpp
+++ polynomial/map_main.cpp
@@ -79,35 +79,36 @@ ostream& operator<<(ostream& os, polynomial p) {
 };
 
 
-int main () {
+int main (int argc, const char * argv[]) {
     ifstream infile;
     ofstream outfile;
-    infile.open ("/Users/deep/desktop/input.txt");
+    infile.open (argv[1]);               //"/Users/deep/desktop/input.txt");
+    outfile.open (argv[2]);
     string line1, line2;
     int x, y;
     while (getline (infile, line1) ) {
-        cout << "original  ";
+        outfile << "original  ";
         istringstream iss (line1);
         polynomial p1;
         while (iss >> x >> y) {
-            cout << x << " " << y << " ";
+            outfile << x << " " << y << " ";
             p1.append (x, y);
         }
-        cout << endl <<  "canonical " << p1;
+        outfile << endl <<  "canonical " << p1;
         
         if (getline(infile, line2)) {
-            cout << "original  ";
+            outfile << "original  ";
             polynomial p2;
             istringstream iss2 (line2);
             while (iss2 >> x >> y) {
-                cout << x << " " << y << " ";
+                outfile << x << " " << y << " ";
                 p2.append(x,y);
             }
-            cout << endl;
-            cout << "canonical  " << p2 ;
-            cout << "sum        " << p1+p2;
-            cout << "difference " << p1-p2;
-            cout << "product    " << p1*p2 << endl;
+            outfile << endl;
+            outfile << "canonical  " << p2 ;
+            outfile << "sum        " << p1+p2;
+            outfile << "difference " << p1-p2;
+            outfile << "product    " << p1*p2 << endl;
         }        
     }
     infile.close();
